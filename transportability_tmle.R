03d999268f7d2613b6ca9616500d6302df4357a4
dzugna <151764197+dzugna@users.noreply.github.com>
2024-02-02 12:36:20 +0100

    Add R code for transportability by TMLE

---
+++ transportability_tmle.R
@@ -0,0 +1,3 @@
+est
+
+#Note that the results differ slightly from the results reported in the text because the covariate data are generated #by randomly sampling with replacement from the original datasets. 
